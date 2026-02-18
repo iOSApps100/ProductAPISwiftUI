8848183898c3a3143998d4865260e22283814cfd
Vikram Kumar <vikramkumarhbti92@gmail.com>
2026-02-18 12:08:49 +0530

    Initial Commit

---
+++ ProductAPISwiftUI/ProductAPISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductAPISwiftUIApp.swift
+//  ProductAPISwiftUI
+//
+//  Created by Vikram Kumar on 18/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ProductAPISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
